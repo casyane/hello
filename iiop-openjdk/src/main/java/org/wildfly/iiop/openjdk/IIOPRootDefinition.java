wildfly/wildfly

IIOP: remove deprecated security attributes

iiop-openjdk/src/main/java/org/wildfly/iiop/openjdk/IIOPRootDefinition.java
@@ -147,6 +147,7 @@ class IIOPRootDefinition extends PersistentResourceDefinition {
     @Deprecated
     public static final AttributeDefinition ADD_COMPONENT_INTERCEPTOR = new SimpleAttributeDefinitionBuilder(
             Constants.SECURITY_ADD_COMP_VIA_INTERCEPTOR, ModelType.BOOLEAN, true)
+            .setDeprecated(IIOPExtension.MODEL_VERSION_1_1_0)
             .setAttributeGroup(Constants.SECURITY)
             .setDefaultValue(new ModelNode(true))
             .setFlags(AttributeAccess.Flag.RESTART_ALL_SERVICES)
@@ -157,6 +158,7 @@ class IIOPRootDefinition extends PersistentResourceDefinition {
     @Deprecated
     public static final AttributeDefinition CLIENT_SUPPORTS = new SimpleAttributeDefinitionBuilder(
             Constants.SECURITY_CLIENT_SUPPORTS, ModelType.STRING, true)
+            .setDeprecated(IIOPExtension.MODEL_VERSION_1_1_0)
             .setAttributeGroup(Constants.SECURITY)
             .setDefaultValue(new ModelNode().set(SSLConfigValue.MUTUALAUTH.toString()))
             .setValidator(SSL_CONFIG_VALIDATOR)
@@ -178,6 +180,7 @@ class IIOPRootDefinition extends PersistentResourceDefinition {
     @Deprecated
     public static final AttributeDefinition SERVER_SUPPORTS = new SimpleAttributeDefinitionBuilder(
             Constants.SECURITY_SERVER_SUPPORTS, ModelType.STRING, true)
+            .setDeprecated(IIOPExtension.MODEL_VERSION_1_1_0)
             .setAttributeGroup(Constants.SECURITY)
             .setDefaultValue(new ModelNode().set(SSLConfigValue.MUTUALAUTH.toString()))
             .setValidator(SSL_CONFIG_VALIDATOR)
@@ -189,6 +192,7 @@ class IIOPRootDefinition extends PersistentResourceDefinition {
     @Deprecated
     public static final AttributeDefinition SERVER_REQUIRES = new SimpleAttributeDefinitionBuilder(
             Constants.SECURITY_SERVER_REQUIRES, ModelType.STRING, true)
+            .setDeprecated(IIOPExtension.MODEL_VERSION_1_1_0)
             .setAttributeGroup(Constants.SECURITY)
             .setDefaultValue(new ModelNode().set(SSLConfigValue.NONE.toString()))
             .setValidator(SSL_CONFIG_VALIDATOR)

casyane/hello

iiop-openjdk/src/main/java/org/wildfly/iiop/openjdk/IIOPRootDefinition.java
@@ -152,6 +152,7 @@ class IIOPRootDefinition extends PersistentResourceDefinition {
     @Deprecated
     public static final AttributeDefinition ADD_COMPONENT_INTERCEPTOR = new SimpleAttributeDefinitionBuilder(
             Constants.SECURITY_ADD_COMP_VIA_INTERCEPTOR, ModelType.BOOLEAN, true)
+            .setDeprecated(IIOPExtension.VERSION_1)
             .setAttributeGroup(Constants.SECURITY)
             .setDefaultValue(new ModelNode(true))
             .setFlags(AttributeAccess.Flag.RESTART_ALL_SERVICES)
@@ -162,6 +163,7 @@ class IIOPRootDefinition extends PersistentResourceDefinition {
     @Deprecated
     public static final AttributeDefinition CLIENT_SUPPORTS = new SimpleAttributeDefinitionBuilder(
             Constants.SECURITY_CLIENT_SUPPORTS, ModelType.STRING, true)
+            .setDeprecated(IIOPExtension.VERSION_1)
             .setAttributeGroup(Constants.SECURITY)
             .setDefaultValue(new ModelNode().set(SSLConfigValue.MUTUALAUTH.toString()))
             .setValidator(SSL_CONFIG_VALIDATOR)
@@ -172,6 +174,7 @@ class IIOPRootDefinition extends PersistentResourceDefinition {
 
     public static final AttributeDefinition CLIENT_REQUIRES = new SimpleAttributeDefinitionBuilder(
             Constants.SECURITY_CLIENT_REQUIRES, ModelType.STRING, true)
+            .setDeprecated(IIOPExtension.VERSION_1)
             .setAttributeGroup(Constants.SECURITY)
             .setDefaultValue(new ModelNode().set(SSLConfigValue.NONE.toString()))
             .setValidator(SSL_CONFIG_VALIDATOR)
@@ -183,6 +186,7 @@ class IIOPRootDefinition extends PersistentResourceDefinition {
     @Deprecated
     public static final AttributeDefinition SERVER_SUPPORTS = new SimpleAttributeDefinitionBuilder(
             Constants.SECURITY_SERVER_SUPPORTS, ModelType.STRING, true)
+            .setDeprecated(IIOPExtension.VERSION_1)
             .setAttributeGroup(Constants.SECURITY)
             .setDefaultValue(new ModelNode().set(SSLConfigValue.MUTUALAUTH.toString()))
             .setValidator(SSL_CONFIG_VALIDATOR)
@@ -194,6 +198,7 @@ class IIOPRootDefinition extends PersistentResourceDefinition {
     @Deprecated
     public static final AttributeDefinition SERVER_REQUIRES = new SimpleAttributeDefinitionBuilder(
             Constants.SECURITY_SERVER_REQUIRES, ModelType.STRING, true)
+            .setDeprecated(IIOPExtension.VERSION_1)
             .setAttributeGroup(Constants.SECURITY)
             .setDefaultValue(new ModelNode().set(SSLConfigValue.NONE.toString()))
             .setValidator(SSL_CONFIG_VALIDATOR)